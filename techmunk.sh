7e5edfd5e998b1976171f1fab49585cc5de198b8
Christian Dale <cdale@aspedia.net>
2025-06-26 16:21:01 +1000

    fix: make build script more robust and clear for our purposes

--- techmunk.sh
+++ techmunk.sh
@@ -1,13 +1,27 @@
-cd Dockerfiles/oc-dev
-docker build . -t ocbuild --target oc-dev-edk2
+#!/bin/bash
 
-cd ../../
+docker build Dockerfiles/oc-dev -t ocbuild --target oc-dev-edk2
+
+mkdir -p build
+rm -rf build/*
+
+# Remove UDK for a clean build
+#sudo rm -rf UDK
 
 docker run \
   -t \
   --volume .:/oc \
+  --volume ./build:/build \
   --env ARCHS=X64 \
   --env TARGETS=RELEASE \
   --env TOOLCHAINS=CLANGDWARF \
   ocbuild \
-  /oc/build_duet.tool
+    bash -c "
+      # Make boot0 and boot1 files
+      DESTDIR=/build make -C /oc/Legacy/BootLoader
+
+      # Build boot program
+      bash /oc/build_duet.tool
+      cp /oc/UDK/Build/OpenDuetPkg/RELEASE_CLANGDWARF/X64/boot* /build
+    "
+
